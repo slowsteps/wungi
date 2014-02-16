ffa65ba413d45b8ca8e58fc2dda62f44a6d9ed63
peter <hofstedemail@yahoo.com>
2014-02-16 11:04:06 +0100

    featured cat

--- index.php
+++ index.php
@@ -4,8 +4,28 @@
 get_header();
 
 
-while ( have_posts() ) : the_post();
-	
+if ( is_front_page() ) 
+{
+	$query = new WP_Query( 'category_name=featured' );
+	while ( $query->have_posts() ) 
+	{
+		$query->the_post();
+		RenderPost();
+	}
+}
+else
+{
+	while( have_posts() )
+	{
+		the_post();
+		RenderPost();
+	}
+
+}
+
+
+function RenderPost()
+{
 	echo '<div class=titledeluxe>';
 		the_post_thumbnail();
 		$subtitle = get_post_meta( get_the_ID(), 'subtitle', true );
@@ -18,8 +38,7 @@ while ( have_posts() ) : the_post();
 	echo '<div class="post">';
 		the_content();
 	echo '</div>';
-	
-endwhile;
+}
 
 get_footer();
 
