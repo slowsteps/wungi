8c458880bc7dfaa747efcee744c38bf7e5e50042
peter <hofstedemail@yahoo.com>
2014-02-18 17:00:28 +0100

    css polish

--- footer.php
+++ footer.php
@@ -3,6 +3,12 @@
 <div class="nav-previous alignleft"><?php if (!is_front_page()) next_posts_link( 'Older posts' ); ?></div>
 <div class="nav-next alignright"><?php if (!is_front_page()) previous_posts_link( 'Newer posts' ); ?></div>
 
+<?php
+
+if (is_single()) next_posts_link();
+
+?>
+
 <?php wp_footer(); ?>
 </body>
 
